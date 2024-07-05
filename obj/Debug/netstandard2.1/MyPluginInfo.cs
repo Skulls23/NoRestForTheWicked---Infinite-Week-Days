b6d96f92160596de192633a576a12d2ea1faa8f3
Florian Boireau <florian23russie@hotmail.fr>
2024-07-05 17:35:16 +0200

    Switched to Harmony instead of Monomod to avoid my code to get erased

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace NoRestForTheWicked___Infinite_Week_Days
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Skulls23.NoRestForTheWicked";
+        public const string PLUGIN_NAME = "NoRestForTheWicked - Infinite Week Days";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
